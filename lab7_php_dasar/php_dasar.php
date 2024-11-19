a664e9c6bf32b5f649bf9585adb1b4264cfcb96d
wirarya2005 <wirarya.tjoa@gmail.com>
2024-11-19 09:33:17 +0700

    file

---
+++ lab7_php_dasar/php_dasar.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dasar</title>
+</head>
+
+<body>
+    <!-- 
+    <h1>Belajar PHP Dasar</h1>
+
+    <?php
+        echo "Hello World" . "<br>";
+    ?>
+
+    <h1>Menggunakan Variable</h1>
+
+    <?php
+        $nim = "312310224";
+        $nama = "Arya Wiratama";
+        echo "NIM : " . $nim . "<br>";
+        echo "Nama : " . $nama;
+    ?> -->
+
+    <!-- <h1>Predefine Variable</h1>
+
+    <?php
+        echo "Selamat Datang " . $_GET['nama'];
+    ?> -->
+
+
+
+    <!-- <form method="post">
+        <label>Nama: </label>
+        <input type="text" name="nama">
+        <input type="submit" value="Kirim">
+    </form>
+
+    <?php
+        echo 'Selamat Datang ' . $_POST['nama'];
+    ?> -->
+
+
+    <!-- <?php
+        $gaji = 1000000;
+        $pajak = 0.1;
+        $thp = $gaji - ($gaji*$pajak);
+        echo "Gaji sebelum pajak = Rp. $gaji <br>";
+        echo "Gaji yang dibawa pulang = Rp. $thp";
+    ?> -->
+
+
+    <!-- <?php
+        $nama_hari = date("l");
+        if ($nama_hari == "Sunday") {
+        echo "Minggu";
+        } elseif ($nama_hari == "Monday") {
+        echo "Senin";
+        } else {
+        echo "Selasa";
+        }
+    ?> -->
+
+    <!-- <?php
+        $nama_hari = date("l");
+        switch ($nama_hari) {
+            case "Sunday":
+                echo "Minggu";
+                break;
+            case "Monday":
+                echo "Senin";
+                break;
+            case "Tuesday":
+                echo "Selasa";
+                break;
+            default:
+                echo "Sabtu";
+        }
+
+    ?> -->
+
+
+    <!-- <?php
+        echo "Perulangan 1 sampai 10 <br />";
+        for ($i=1; $i<=10; $i++) {
+            echo "Perulangan ke: " . $i . '<br />';
+        }
+
+        echo "Perulangan Menurun dari 10 ke 1 <br />";
+        for ($i=10; $i>=1; $i--) {
+            echo "Perulangan ke: " . $i . '<br />';
+        }
+    ?> -->
+
+    <!-- <?php
+        echo "Perulangan 1 sampai 10 <br />";
+        $i=1;
+        while ($i<=10) {
+            echo "Perulangan ke: " . $i . '<br />';
+            $i++;
+        }
+    ?> -->
+
+    <?php
+        echo "Perulangan 1 sampai 10 <br />";
+        $i=1;
+        do {
+            echo "Perulangan ke: " . $i . '<br />';
+            $i++;
+        } while ($i<=10);
+    ?>
+
+</body>
+
+</html>
